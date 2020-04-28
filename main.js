e8142f3910b45ed88e93849f39f1b237f5c96809
ticet11 <BriKozub@gmail.com>
2020-04-28 13:36:44 -0600

    Compound Conditionals in JavaScript

--- main.js
+++ main.js
@@ -213,3 +213,21 @@ if (age >= 25 && age < 80) {
 } else {
   console.log('You are an old person!')
 }
+
+// We recognize that there is a hole is this code logic covering ages 11 -15. As was discussed in the lesson this does happen.
+// Try playing with the code and fixing the hole in the logic for practice.
+var age = 30;
+
+if (age <= 10) {
+  console.log("You can eat from the kid's menu");
+  console.log("You are not old enough to drive");
+  console.log("You are not old enough to rent a car");
+} else if (age >= 16 && age < 25) {
+  console.log("You can not eat from the kid's menu");
+  console.log("You are old enough to drive");
+  console.log("You are not old enough to rent a car");
+} else if (age >= 25) {
+  console.log("You can not eat from the kid's menu");
+  console.log("You are old enough to drive");
+  console.log("You are old enough to rent a car");
+}
