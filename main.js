e75e424f3bc68cd2f47b38bb2c6225ecec240de4
ticet11 <BriKozub@gmail.com>
2020-04-28 11:40:25 -0600

    Order of Operations in JavaScript

--- main.js
+++ main.js
@@ -160,3 +160,17 @@ sum; // 100
 sum += gradeTwo; // 180
 
 sum *= gradeTwo; // 14400
+
+/* Order of Operations
+   PEMDAS -> PEDMAS
+   Please Excuse My Dear Aunt Sally
+   Paranthesis
+   Exponents
+   Multiplication 5 / 5 * 4
+   Division
+   Addition
+   Subtraction
+*/
+
+var num = 5 + (5 * 10) / 6**2 - 1;
+var num = 5 / 5 * 4;
