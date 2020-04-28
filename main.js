d621bc8b0548f1fb62ef5370836d0a0cea7e19f8
ticet11 <BriKozub@gmail.com>
2020-04-28 12:53:48 -0600

    Basic Syntax for Using Conditionals in JavaScript

--- main.js
+++ main.js
@@ -174,3 +174,24 @@ sum *= gradeTwo; // 14400
 
 var num = 5 + (5 * 10) / 6**2 - 1;
 var num = 5 / 5 * 4;
+
+/* Conditionals */
+
+var age = 10;
+var ageTwo = '12';
+
+if (age === ageTwo) {
+  console.log('They are equal');
+}
+
+if (age !== ageTwo) {
+  console.log('Not equal');
+}
+
+if (age >= 25) {
+  console.log('Old enough to rent a car');
+}
+
+if (age <= 10) {
+  console.log('You can eat from the kid menu');
+}
