727b28a08667812384191b2514cbfea31b4ef7ac
ticet11 <BriKozub@gmail.com>
2020-04-29 15:20:10 -0600

    Guide to For Loops in JavaScript

--- main.js
+++ main.js
@@ -487,3 +487,24 @@ arr; // ["Altuve", "Bregman", "Springer"]
 arr.splice(1, 2); // ["Bregman", "Springer"]
 
 arr; // ["Altuve"]
+
+/* Loops */
+
+var players = [
+    'Altuve',
+    'Bregman',
+    'Correa',
+    'Springer'
+  ];
+  
+  for (player in players) {
+    console.log(players[player]);
+  }
+  
+  for (var i = 0; i < players.length; i++) {
+    console.log(players[i]);
+  }
+  
+  players.forEach(function(element) {
+    console.log(element);
+  });
