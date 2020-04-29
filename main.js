571eab5050670b9c5702a08349808fd6a34d2645
ticet11 <BriKozub@gmail.com>
2020-04-29 15:36:14 -0600

    How to Loop Through a JavaScript Object

--- main.js
+++ main.js
@@ -508,3 +508,13 @@ var players = [
   players.forEach(function(element) {
     console.log(element);
   });
+
+  var student = {
+    name: 'Kristine',
+    age: 12,
+    city: 'Scottsdale'
+  };
+  
+  for (var key in student) {
+    console.log(key + " => " + student[key]);
+  }
