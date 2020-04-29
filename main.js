45759c14634988bc7b1240432d457bef66942df1
ticet11 <BriKozub@gmail.com>
2020-04-29 14:31:15 -0600

    How to Use the Splice Function in JavaScript to Remove Specific Array Elements

--- main.js
+++ main.js
@@ -473,3 +473,17 @@ arr.shift(); // ['Bregman', 'Correa', 'Bagwell']
 arr.unshift('Kyle'); // 4
 
 arr; // ['Kyle', 'Bregman', 'Correa', 'Bagwell']
+
+var arr = ['Altuve', 'Bregman', 'Correa', 'Springer'];
+
+var foundElement = arr.indexOf('Correa');
+
+foundElement; // 2
+
+arr.splice(foundElement, 1); // ["Correa"]
+
+arr; // ["Altuve", "Bregman", "Springer"]
+
+arr.splice(1, 2); // ["Bregman", "Springer"]
+
+arr; // ["Altuve"]
