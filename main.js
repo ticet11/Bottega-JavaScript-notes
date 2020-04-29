c8f8eaf99c4d98ce82fb27509b7ff255439cdc44
ticet11 <BriKozub@gmail.com>
2020-04-29 14:22:02 -0600

    Guide to Adding and Removing Array Elements in JavaScript

--- main.js
+++ main.js
@@ -455,3 +455,21 @@ var guide = {
 
 user = { role: 'paid' };
 guide.renderContent(user.role);
+
+/* Adding and removing array elements */
+
+var arr = ['Altuve', 'Bregman', 'Correa', 'Springer'];
+
+arr.pop(); // "Springer"
+
+arr; // ['Altuve', 'Bregman', 'Correa']
+
+arr.push('Bagwell'); // 4
+
+arr; // ['Altuve', 'Bregman', 'Correa', 'Bagwell']
+
+arr.shift(); // ['Bregman', 'Correa', 'Bagwell']
+
+arr.unshift('Kyle'); // 4
+
+arr; // ['Kyle', 'Bregman', 'Correa', 'Bagwell']
