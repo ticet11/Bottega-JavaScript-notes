fd9a5a36057dac308f07f50786e92110b8100739
ticet11 <BriKozub@gmail.com>
2020-04-29 11:25:16 -0600

    Function Arguments: Reference vs Value

--- main.js
+++ main.js
@@ -370,3 +370,39 @@ function fullName(firstName, lastName, language = "English") {//cleaner and cool
 }
 
 console.log(fullName("Jordan", "Hudgens", "Spanish"));
+
+/* function arguments */
+
+var someUser = {
+  name: 'Jordan'
+}
+
+function nameFormatter (user) {
+  return user.name = 'Oops';
+}
+
+nameFormatter(someUser); // "Oops"
+
+someUser; // Object {name: "Oops"}
+
+var someName = 'Tiffany';
+
+function someOtherNameFormatter(name) {
+  return name = 'Oops';
+}
+
+someOtherNameFormatter(someName); // "Oops"
+
+someName; // "Tiffany"
+
+function nameFormatter (userName) {
+  return userName = 'Oops';
+}
+
+someUser.name; // "Oops"
+
+someUser.name = 'Kristine';
+
+nameFormatter(someUser.name); // "Oops"
+
+someUser.name; // "Kristine"
