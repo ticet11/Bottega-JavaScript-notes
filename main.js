ed3ae1cd3da6dd639fd0ea5a87adcbf16a9760cb
ticet11 <BriKozub@gmail.com>
2020-04-30 16:05:03 -0600

    Guide to Adding Default Object Values to JavaScript Function Arguments

--- main.js
+++ main.js
@@ -628,3 +628,19 @@ const renderUser = ({ name, email }) => {
 }
 
 renderUser(user);
+
+/* Default object values */
+
+const blog = {
+  title: 'My great post',
+  summary: 'Summary of my post'
+}
+
+const openGraphMetadata = ({ title, summary = 'A DailySmarty Post' }) => {
+  console.log(`
+    og-title=${title}
+    og-description=${summary}
+  `);
+}
+
+openGraphMetadata(blog);
