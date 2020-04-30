66116c3f339c2617e36e527dc052a1c76daaa34b
ticet11 <BriKozub@gmail.com>
2020-04-30 17:03:12 -0600

    How to Check if Two Objects Have Equal Values

--- main.js
+++ main.js
@@ -706,3 +706,43 @@ const tiffany = fullName.bind(userTwo);
 
 kristine();
 tiffany();
+
+/* compare two objects */
+
+const isEqual = (obj1, obj2) => {
+  const obj1Keys = Object.keys(obj1);
+  const obj2Keys = Object.keys(obj2);
+
+  if (obj1Keys.length !== obj2Keys.length) {
+    return false;
+  }
+
+  for (let objKey of obj1Keys) {
+    if (obj1[objKey] !== obj2[objKey]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const obj1 = {
+  name: "Kristine",
+  age: 13,
+  favorites: {
+    food: "Pizza",
+    vacation: "Disneyland"
+  }
+};
+
+const obj2 = {
+  name: "Kristine",
+  age: 13,
+  favorites: {
+    food: "Pizza",
+    vacation: "Disneyland"
+  }
+};
+
+obj1 == obj2;
+isEqual(obj1, obj2);
