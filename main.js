b62c7e922e100f855510cd76c3a4696652b314a5
ticet11 <BriKozub@gmail.com>
2020-04-30 15:42:06 -0600

    How to Implement Array Destructuring in JavaScript

--- main.js
+++ main.js
@@ -601,3 +601,17 @@ console.log(`
 Player One: ${playerOne}
 Player Two: ${playerTwo}
 `);
+
+/* Array destructuring */
+
+const apiList = [
+  'https://api.dailysmarty.com/posts',
+  'https://api.github.com/users/jordanhudgens/repos',
+  'https://api.github.com/users/jordanhudgens'
+]
+
+const [posts, repos, profile] = apiList;
+
+console.log(posts);
+console.log(repos);
+console.log(profile);
