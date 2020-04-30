68ac8a058c91fab26e6c673fb4f304b930c9dc46
ticet11 <BriKozub@gmail.com>
2020-04-30 10:27:17 -0600

    Introduction to Arrow Functions in JavaScript

--- main.js
+++ main.js
@@ -542,3 +542,29 @@ var players = [
 
 const page = 'about';
 console.log(`class=${ page === 'Home' ? 'master-layout' : 'secondary-layout' }`);
+
+/* Arrow Functions */
+
+// Same function written as function declaration
+function fullName (fName, lName) { 
+  console.log(`${lName}, ${fName}`);
+}
+fullName('Tiffany', 'Hudgens');
+
+// Same function written as function expression
+fullName = (fName, lName) => { 
+  console.log(`${lName}, ${fName}`);
+}
+fullName('Kristine', 'Hudgens');
+
+// Basic arrow function
+helloWorld = () => { console.log("Hi there"); }
+helloWorld();
+
+// Arrow function with shorthand function argument for single arguments
+firstName = fname => { console.log(fname.toUpperCase()); }
+firstName('Jordan');
+
+// Arrow function with multiple arguments
+fullName = (fName, lName) => { console.log(`${lName}, ${fName}`); }
+fullName('Kristine', 'Hudgens');
