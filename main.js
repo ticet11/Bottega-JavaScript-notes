cb5bb5cb800ff6669adbc5644192b4365bca8c13
ticet11 <BriKozub@gmail.com>
2020-04-30 16:47:29 -0600

    Guide to JavaScript Bind Function

--- main.js
+++ main.js
@@ -678,3 +678,31 @@ const { starter, closer, ...relievers } = {
 console.log(starter);
 console.log(closer);
 console.log(relievers);
+
+/* Bind */
+
+const userOne = {
+  firstName: "Kristine",
+  lastName: "Hudgens"
+};
+
+const userTwo = {
+  firstName: "Tiffany",
+  lastName: "Hudgens"
+};
+
+// Function expression
+const fullName = function() {
+  return `${this.lastName}, ${this.firstName}`;
+};
+
+// Nope!
+// const fullName = () => {
+//   return `${this.lastName}, ${this.firstName}`;
+// };
+
+const kristine = fullName.bind(userOne);
+const tiffany = fullName.bind(userTwo);
+
+kristine();
+tiffany();
