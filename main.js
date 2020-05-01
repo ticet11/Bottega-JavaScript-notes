1edc429b47db44c788017850847176e5ca5f944e
ticet11 <BriKozub@gmail.com>
2020-05-01 15:29:51 -0600

    Group promises with promise.all

--- main.js
+++ main.js
@@ -837,3 +837,22 @@ sleepyGreeting
   .catch(err => {
     console.error(err);
   });
+
+  /* Group promises together */
+
+  const greeting = new Promise((resolve, reject) => {
+    resolve('Hi, there.');
+    reject('Oops, bad greeting.');
+  });
+  
+  const updateAccount = new Promise((resolve, reject) => {
+    resolve('Updating login...');
+    reject('Error updating account with login.');
+  });
+  
+  const loginActivities = Promise.all([greeting, updateAccount]);
+  loginActivities.then(res => {
+    res.forEach(activity => {
+      console.log(activity);
+    })
+  })
