3fc440367bf59c45c4b06aea3197093e63115643
ticet11 <BriKozub@gmail.com>
2020-05-01 15:50:26 -0600

    Combining Async / Await with Closures to Ensure All Processes Have Run

--- main.js
+++ main.js
@@ -856,3 +856,31 @@ sleepyGreeting
       console.log(activity);
     })
   })
+
+  /* Promises and closures */
+
+  const login = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('User logged in...');
+      }, 4000);
+    });
+  }
+  
+  const updateAccount = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('Updating last login...');
+      }, 2000);
+    });
+  }
+  
+  async function loginActivities(login, updateAccount) {
+    const returnedLogin = await login;
+    console.log(returnedLogin);
+  
+    const returnedUpdateAccount = await updateAccount;
+    console.log(returnedUpdateAccount);
+  }
+  
+  loginActivities(login(), updateAccount());
