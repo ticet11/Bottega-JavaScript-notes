fde448f280c323f4b02470473d88ba34728a90df
ticet11 <BriKozub@gmail.com>
2020-05-01 13:57:29 -0600

    Introduction to JavaScript Promises

--- main.js
+++ main.js
@@ -817,3 +817,23 @@ console.log(
 console.log(
   `${seniorInstructor.name} can teach: ${Instructor.canTeach(seniorInstructor)}`
 );
+
+/* Promises */
+
+let sleepyGreeting = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Hello....')
+  }, 2000);
+
+  setTimeout(() => {
+    reject(Error('Too sleepy...'))
+  }, 2000);
+});
+
+sleepyGreeting
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.error(err);
+  });
