fed0fb8a5017d01d79904a73664ac3921dd7882c
ticet11 <BriKozub@gmail.com>
2020-05-01 11:53:27 -0600

    Guide to OOP Static Methods in JavaScript

--- main.js
+++ main.js
@@ -775,3 +775,45 @@ const benny = new Instructor({ name: 'Benny Kozub', cohort: 'April 7' });
 
 brian.renderDetails();
 benny.renderDetails();
+
+/* static methods */
+
+class Instructor {
+  constructor({ name, role = "assistant" }) {
+    this.role = role;
+    this.name = name;
+  }
+
+  // Instance method
+  renderDetails() {
+    console.log(`${this.name} - ${this.role}`);
+  }
+
+  // Base case static method
+  static helloWorld() {
+    console.log('Hi there');
+  }
+
+  // Static method
+  static canTeach(instructor) {
+    return (instructor.role === 'classroom');
+  }
+}
+
+let juniorInstructor = new Instructor({ 'name' : 'Brian' });
+let seniorInstructor = new Instructor({ 'name' : 'Alice', "role" : "classroom" });
+
+juniorInstructor.renderDetails(); // "Brian - assistant"
+seniorInstructor.renderDetails(); // "Alice - classroom"
+
+Instructor.helloWorld(); // "Hi there"
+
+// "Brian can teach: false"
+console.log(
+  `${juniorInstructor.name} can teach: ${Instructor.canTeach(juniorInstructor)}`
+);
+
+// "Alice can teach: true"
+console.log(
+  `${seniorInstructor.name} can teach: ${Instructor.canTeach(seniorInstructor)}`
+);
