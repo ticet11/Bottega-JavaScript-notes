0ef5f97efdb3bd3f923aac966862096e4b915670
ticet11 <BriKozub@gmail.com>
2020-05-04 15:18:08 -0600

    How to Create a Custom Error Class in JavaScript

--- errorManagement/main.js
+++ errorManagement/main.js
@@ -1,13 +1,23 @@
+class DevcampError extends Error {
+    constructor(message = "An error occured!", ...params) {
+        super(...params);
+
+        this.message = message;
+    }
+}
+
 function siteComponent(func) {
     return func();
 }
 
-widget = () => '<div>Hi there</div>';
-
+widget = () => "<div>Hi there</div>";
 
 try {
-    console.log(siteComponent('widget'));
-} catch(e) {
+    console.log(siteComponent(widget));
+} catch (e) {
+    throw new DevcampError("DevcampError", e);
     // console.error('You got a problem, buddy.', e);
-    document.getElementById('hello').innerHTML = 'You got a problem, buddy.';
+    // document.getElementById('hello').innerHTML = 'You got a problem, buddy.';
 }
+
+
