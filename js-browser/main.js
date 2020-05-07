195b8c8cc2fd0a19e5f9b58a554f528d294ebcd2
Brian Kozub <ticet11@gmail.com>
2020-05-07 16:50:56 -0600

    How to Add and Remove Classes in Vanilla JavaScript

--- js-browser/main.js
+++ js-browser/main.js
@@ -1,12 +1,16 @@
-// DOM -> Document Object Model
+const heading = document.getElementById('main-heading');
 
-const user = {
-    name: 'Brian',
-    email: 'brian@briankozub.com',
-    favorites: [
-        'MacDonald',
-        "Cubby's"
-    ]
+// heading.onclick = () => {
+//     heading.classList.contains('active-heading') ?
+//         heading.classList.remove('active-heading') 
+//         & heading.classList.add('non-active-heading') 
+//         : heading.classList.remove('non-active-heading') 
+//         & heading.classList.add('active-heading');
+// }
+
+// // Will totally override all classes.
+heading.onclick = () => {
+    heading.classList.contains('active-heading') ?
+        heading.className = 'non-active-heading'
+        : heading.className = 'active-heading';
 }
-
-user.favorites[1];
